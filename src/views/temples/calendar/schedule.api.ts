4fdd81a0d94808ff3d565cb3f8b1f3e215a9dc87
steven <mrjiangyan@hotmail.com>
2023-10-12 09:13:53 +0800

    接口地址错误

--- src/views/temples/calendar/schedule.api.ts
+++ src/views/temples/calendar/schedule.api.ts
@@ -3,13 +3,12 @@ import { defHttp } from '/@/utils/http/axios';
 const tableId = 'cb26678c67b4465aa17a8a99e28c256a';
 
 export enum Api {
-  scheduleList = '/api/temples/client/schedule/list',
-  disable = '/api/temples/client/schedule/disable/',
-  enabled = '/api/temples/client/schedule/enabled/',
+  scheduleList = '/api/temples/schedule/list',
+  disable = '/api/temples/schedule/disable/',
+  enabled = '/api/temples/schedule/enabled/',
   save = '/online/cgform/api/form/' + tableId,
   get = '/online/cgform/api/form/',
 
-  searchOrder = '/api/temples/order/search',
   assign = '/api/temples/tablets/assign/byOrder',
 }
 
@@ -30,28 +29,7 @@ export const getById = (id) => {
 };
 
 /**
- * 获取牌位位置列表
- */
-export const queryTreeSync = (params?) => defHttp.get({ url: Api.queryTreeSync, params });
-
-/**
- * 获取牌位位置列表
- */
-export const searchOrder = (params?) => {
-  params.bizCode = 'tablets';
-  return defHttp.get({ url: Api.searchOrder, params });
-};
-
-/**
- * 保存或者更新示例
- * @param params
- */
-export const saveOrUpdate = (params, isUpdate) => {
-  return isUpdate ? defHttp.put({ url: Api.save, params }) : defHttp.post({ url: Api.save, params });
-};
-
-/**
- * 禁用某一个活动schedule
+ * 启用某一个活动schedule
  * @param params
  */
 export const enabled = (id, handleSuccess) => {
